29840dcf28ebf40235fa562d32772291ff45f86d
itsmo1031 <itsmo1031@gmail.com>
2023-03-29 15:49:12 +0900

    🎉 feat: Initial Commit

---
+++ src/test/java/dev/itsmo/springcorebasic/SpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.itsmo.springcorebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
