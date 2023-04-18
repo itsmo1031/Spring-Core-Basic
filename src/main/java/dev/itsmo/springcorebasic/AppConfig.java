be92626e83df5fdf6fc7beb8acd30327277d06ea
itsmo1031 <itsmo1031@gmail.com>
2023-04-18 16:53:22 +0900

    ✨ feat: turn Pure Java code into Spring

--- src/main/java/dev/itsmo/springcorebasic/AppConfig.java
+++ src/main/java/dev/itsmo/springcorebasic/AppConfig.java
@@ -8,21 +8,28 @@ import dev.itsmo.springcorebasic.member.MemberServiceImpl;
 import dev.itsmo.springcorebasic.member.MemoryMemberRepository;
 import dev.itsmo.springcorebasic.order.OrderService;
 import dev.itsmo.springcorebasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
