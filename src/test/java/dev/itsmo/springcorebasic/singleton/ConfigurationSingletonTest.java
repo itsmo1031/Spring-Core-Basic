24fb1d96ec9801aef3c06d002f8321685fcefa82
itsmo1031 <itsmo1031@gmail.com>
2023-04-19 16:55:13 +0900

    ✅ test: add test for figuring Configuration is singleton

---
+++ src/test/java/dev/itsmo/springcorebasic/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,33 @@
+package dev.itsmo.springcorebasic.singleton;
+
+import dev.itsmo.springcorebasic.AppConfig;
+import dev.itsmo.springcorebasic.member.MemberRepository;
+import dev.itsmo.springcorebasic.member.MemberServiceImpl;
+import dev.itsmo.springcorebasic.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        // 모두 같은 인스턴스를 참조하고 있음
+        assertThat(memberRepository).isSameAs(memberRepository1).isSameAs(memberRepository2);
+    }
+}
