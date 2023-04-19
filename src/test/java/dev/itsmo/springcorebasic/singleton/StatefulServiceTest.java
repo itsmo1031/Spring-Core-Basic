fc69c864d00753190d3baba7e23d6488b9566d73
itsmo1031 <itsmo1031@gmail.com>
2023-04-19 16:43:07 +0900

    ✅ test: add test for stateful service

---
+++ src/test/java/dev/itsmo/springcorebasic/singleton/StatefulServiceTest.java
@@ -0,0 +1,36 @@
+package dev.itsmo.springcorebasic.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+class StatefulServiceTest {
+    @Test
+    void statefulServiceSingleton() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // Thread A: A 사용자 10_000원 주문
+        statefulService1.order("userA", 10000);
+        // Thread B: B 사용자 20_000원 주문
+        statefulService2.order("userB", 20000);
+
+        // Thread A: 사용자 A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
